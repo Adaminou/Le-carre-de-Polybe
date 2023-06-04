b4e47c82c4dabbb89be373051e8fff310d6239c6
Adaminou <200537@site.asty-moulin.com>
2023-06-05 00:55:59 +0200

    FinalisationDuJeu

---
+++ 5T23_EssalhiAdam_CarrePolybe/MethodeTraitementPolybe.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5T23_EssalhiAdam_CarrePolybe
+{
+    public struct MethodeTraitementMat
+    {
+        public void Cryptage(string phrase, out string[,] chiffre, string[,] matricesDeBase)
+        {
+            chiffre = new string[phrase.Length, 2];
+            for (int a = 0; a < phrase.Length; a++)
+            {
+                for (int i = 1; i < 6; i++)
+                {
+                    for (int y = 1; y < 6; y++)
+                    {
+                        if (phrase[a].ToString() == "J" && matricesDeBase[i, y] == "I")
+                        {
+                            chiffre[a, 0] = matricesDeBase[0, i];
+                            chiffre[a, 1] = matricesDeBase[y, 0];
+                        }
+                        else if (phrase[a].ToString() == matricesDeBase[i, y])
+                        {
+                            chiffre[a, 0] = matricesDeBase[0, i];
+                            chiffre[a, 1] = matricesDeBase[y, 0];
+                        }
+                    }
+                }
+            }
+        }
+        public void DeCryptage(string phrase, out string codeDechiffrer, string[,] matricesDeBase, string[,] chiffre)
+        {
+            codeDechiffrer = "";
+            for (int a = 0; a < chiffre.GetLength(0); a++)
+            {
+                for (int i = 1; i < 6; i++)
+                {
+                    for (int y = 1; y < 6; y++)
+                    {
+
+                        if (chiffre[a, 0] == matricesDeBase[0, i] && chiffre[a, 1] == matricesDeBase[y, 0])
+                        {
+                            codeDechiffrer += matricesDeBase[i, y];
+
+                        }
+                    }
+                }
+            }
+        }
+        public string RetireEspaces(string chaine)
+        {
+            string newChaine = "";
+            string carExt;
+            int lg = chaine.Length;
+            for (int i = 0; i < lg; i++)
+            {
+                carExt = chaine.Substring(i, 1);
+                if (carExt != " ")
+                {
+                    newChaine += carExt;
+                }
+            }
+            return newChaine;
+        }
+        public void CreationMatrice(string code, out string[,] matricesDeBase)
+        {
+            int ligne = 1;
+            int colonne = 1;
+
+            string[] alphabet = new string[25] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", };
+            matricesDeBase = new string[6, 6];
+            for (int i = 0; i < 6; i++)
+            {
+                matricesDeBase[0, i] = i.ToString();
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                matricesDeBase[i, 0] = i.ToString();
+            }
+
+            for (int codeInt = 0; codeInt < code.Length; codeInt++)
+            {
+
+                for (int alphabetInt = 0; alphabetInt < 25; alphabetInt++)
+                {
+                    if (code[codeInt].ToString() == alphabet[alphabetInt])
+                    {
+
+                        matricesDeBase[ligne, colonne] = alphabet[alphabetInt].ToString();
+                        alphabet[alphabetInt] = " ";
+                        if (codeInt != code.Length)
+                        {
+                            colonne++;
+                        }
+
+                        if (colonne == 6 && codeInt != code.Length)
+                        {
+                            ligne++;
+                            colonne = 1;
+                        }
+
+                    }
+                }
+
+            }
+            for (int li = ligne; li < 6; li++)
+            {
+                for (int co = colonne; co < 6; co++)
+                {
+                    for (int alphabetInt = 0; alphabetInt < 25; alphabetInt++)
+                    {
+
+                        if (alphabet[alphabetInt].ToString() != " ")
+                        {
+                            matricesDeBase[li, co] = alphabet[alphabetInt].ToString();
+                            alphabet[alphabetInt] = " ";
+                            alphabetInt = 25;
+                            if (co == 5)
+                            {
+                                colonne = 1;
+                            }
+
+
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < matricesDeBase.GetLength(0); i++)
+            {
+                for (int y = 0; y < matricesDeBase.GetLength(1); y++)
+                {
+                    Console.Write(matricesDeBase[i, y] + "|");
+                }
+                Console.WriteLine(" ");
+            }
+
+        }
+        public bool EntreeEstBonne(string entree)
+        {
+            for (int i = 0; i <= entree.Length - 1; i++) // i = Variable qui s’incrémente dans une boucle for (place dans le string)
+            {
+                int ic = entree[i]; // Valeur du caractère à la place "i" dans le string
+                if (!(ic >= 65 && ic <= 90) && !(ic >= 97 && ic <= 122) && ic != 32) return false;
+            }
+            return true;
+        }
+        public void LireReel(out double n)
+        {
+            string nUser;
+
+            nUser = Console.ReadLine();
+            nUser = RetireEspaces(nUser);
+            while (!double.TryParse(nUser, out n))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Attention ! vous devez entrer un nombre réel !");
+                Console.ResetColor();
+                nUser = Console.ReadLine();
+                nUser = RetireEspaces(nUser);
+            }
+        }
+    }
+}
---
+++ 5T23_EssalhiAdam_CarrePolybe/Program.cs
@@ -0,0 +1,190 @@
+﻿using _5T23_EssalhiAdam_CarrePolybe;
+using System;
+
+namespace testPourArabeN3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "";
+            string[,] chiffre;
+            string codeDechiffrer = "";
+            string[,] matricesDeBase = new string[6, 6];
+            string choix;
+            string entreeUser;
+            string code = "";
+            string chiffres;
+            int n;
+            int compteurL = 0;
+            int compteurC = 0;
+            int compteurTest = 0;
+            bool recom = true;
+
+            MethodeTraitementMat MesOutils = new MethodeTraitementMat();
+
+            while (recom)
+            {
+                compteurL = 0;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("LE CARRE DE POLYBE");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("=========================");
+                Console.ResetColor();
+                MesOutils.CreationMatrice(code, out matricesDeBase);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("=========================");
+                Console.ResetColor();
+
+           
+                Console.WriteLine("Voulez-vous crypter ou décrypter un message ? : ");
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("=========================");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("    1. Crypter");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("=========================");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("    2. Décrypter");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("=========================");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("    3. Quitter");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("=========================");
+                Console.ResetColor();
+
+
+                choix = Console.ReadLine();
+                if (choix == "1")
+                {
+                    Console.WriteLine("Quelle phrase voulez-vous crypter ?");
+                    phrase = Console.ReadLine();
+                    phrase = phrase.ToUpper();
+                    Console.WriteLine("Quel sera votre code ?");
+                    code = Console.ReadLine();
+                    code = MesOutils.RetireEspaces(code);
+                    code = code.ToUpper();
+                    MesOutils.CreationMatrice(code, out matricesDeBase);
+                    MesOutils.Cryptage(phrase, out chiffre, matricesDeBase);
+
+                   
+                    Console.Write("Cryptage : ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    for (int i = 0; i < phrase.Length; i++)
+                    {
+                        for (int y = 0; y < 2; y++)
+                        {
+                            Console.Write(chiffre[i, y]);
+                        }
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                    Console.ResetColor();
+                }
+
+                if (choix == "2")
+                {
+                    Console.WriteLine("Quels chiffres voulez-vous décrypter ?");
+                    MesOutils.LireReel(out double chiffreDecryption);
+                    chiffres = chiffreDecryption.ToString();
+                    chiffres = MesOutils.RetireEspaces(chiffres);
+                    bool validChiffres = true;
+
+                    foreach (char item in chiffres)
+                    {
+                        int chiffreValeur = Convert.ToInt32(item.ToString());
+                        if (chiffreValeur > 5)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Attention ! Vous devez entrer des valeurs inférieures ou égales à 5 !");
+                            Console.ResetColor();
+                            validChiffres = false;
+                        }
+                    }
+
+                    if (chiffres.Length % 2 != 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Attention! Le total des chiffres doit être pair !");
+                        Console.ResetColor();
+                        validChiffres = false;
+                    }
+
+                    if (validChiffres)
+                    {
+                        chiffre = new string[chiffres.Length / 2, 2];
+                        Console.WriteLine("Quel est votre code ?");
+                        code = Console.ReadLine();
+                        code = code.ToUpper();
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("=========================");
+                        Console.ResetColor();
+                        MesOutils.CreationMatrice(code, out matricesDeBase);
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("=========================");
+                        Console.ResetColor();
+                        foreach (char seulChiffre in chiffres)
+                        {
+                            compteurTest++;
+                            chiffre[compteurL, compteurC] = seulChiffre.ToString();
+                            if (compteurC == 0)
+                            {
+                                compteurC = 1;
+                            }
+                            else
+                            {
+                                compteurC = 0;
+                                compteurL++;
+                            }
+                        }
+                        Console.WriteLine("Vos chiffres : " );
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        for (int i = 0; i < chiffre.GetLength(0); i++)
+                        {
+                            for (int y = 0; y < 2; y++)
+                            {
+                                Console.Write(chiffre[i, y]);
+                            }
+                            Console.Write(" ");
+                        }
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("=========================");
+                        Console.ResetColor();
+
+                        MesOutils.DeCryptage(phrase, out codeDechiffrer, matricesDeBase, chiffre);
+                        Console.WriteLine("Votre phrase décryptée : ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(codeDechiffrer);
+                        Console.ResetColor();
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("=========================");
+                        Console.ResetColor();
+                    }
+                }
+                if (choix == "3")
+                {
+                    break;
+                }
+
+                    Console.WriteLine("Voulez vous faire un autre cryptage ? (oui = o / non = autre)");
+                entreeUser = Console.ReadLine();
+                if (entreeUser != "o")
+                {
+                    recom = false;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
